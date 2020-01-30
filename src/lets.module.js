141cc2683a9129352b9a2ca53d5463a40e266653
Vitor Borges <vitorsbgs@gmail.com>
2020-01-30 17:55:34 -0300

    new input type colorpicker

--- src/lets.module.js
+++ src/lets.module.js
@@ -18,7 +18,8 @@
         'colorpicker-dr',
         'ckeditor',
         'thatisuday.dropzone',
-        'swangular'
+        'swangular',
+        'colorpicker.module'
     ]);
 
     // ----------------------------
