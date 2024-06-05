e002eabc4bcff19ad1a9f6c01c01b9deb2875228
Junio de Souza <junioapdesouza@hotmail.com>
2024-06-05 10:38:21 -0300

    revert to 1.5.5

--- src/directives/framework/lets-fw-upload.directive.js
+++ src/directives/framework/lets-fw-upload.directive.js
@@ -100,14 +100,7 @@
                                     $scope.f.alreadySent = true;
                                     $scope.f.uploading = false;
                                     $scope.f.name = response.data.result.files.file[0].name;
-
-                                    if(response.data.result.files.file[0].url != null){
-                                        $scope.f.fileURL = response.data.result.files.file[0].url
-                                        console.log($scope.f.fileURL)
-                                    }else{
-                                        $scope.f.fileURL = STORAGE_URL+$scope.f.name;
-                                    }
-                                    console.log($scope.f.fileURL)
+                                    $scope.f.fileURL = STORAGE_URL+$scope.f.name;
                                     $scope.f.isImage = $scope.isFileImage($scope.f.name)
                                     $scope.f.isVideo = $scope.isFileVideo($scope.f.name)
 
